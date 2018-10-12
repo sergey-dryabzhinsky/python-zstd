2dc2acecc3d429a6b1f5e881fadd7dbba388fe45
Zack Weinberg <zackw@panix.com>
2018-10-12 12:10:15 -0400

    Rewrite the version number tests.
    They no longer care whether the zstd module is using a bundled or
    external libzstd; instead they check for the expected mathematical
    relationships among all of the version-number constants and functions,
    and double-check that both the compile-time and run-time libzstd
    versions are >= 1.3.4, consistent with the build-time check in _zstd.c.

--- tests/base.py
+++ tests/base.py
@@ -12,10 +12,9 @@ log = logging.getLogger("ZSTD")
 log.info("Python version: %s" % sys.version)
 
 ###
-# Compatibility headache: The feature of skipping tests was added to
-# unittest in 2.7.  If necessary, provide a SkipTest exception and a
-# TestZSTD class that shims TestCase.skipTest().  The skip decorators
-# are not shimmed.
+# The feature of skipping tests was added to unittest in 2.7.
+# If necessary, shim TestCase.skipTest and supply a SkipTest
+# exception.  The skip decorators are not shimmed.
 
 if hasattr(unittest, "SkipTest"):
     SkipTest = unittest.SkipTest
@@ -54,25 +53,8 @@ else:
 
 class BaseTestZSTD(BaseTestCase):
 
-    ZSTD_EXTERNAL = False
     LEGACY = False
-    VERSION = ""
-    VERSION_INT = 0
-    VERSION_INT_MIN = 1 * 100*100 + 0 * 100 + 0
-    PKG_VERSION = ""
 
     def setUp(self):
         if os.getenv("LEGACY"):
             self.LEGACY = True
-        if os.getenv("ZSTD_EXTERNAL"):
-            self.ZSTD_EXTERNAL = True
-
-        self.VERSION = os.getenv("VERSION")
-        self.PKG_VERSION = os.getenv("PKG_VERSION")
-        v = [int(n) for n in self.VERSION.split(".")]
-        v = sorted(v, reverse=True)
-        self.VERSION_INT = 0
-        i = 0
-        for n in v:
-            self.VERSION_INT += n * 100**i
-            i += 1
--- tests/test_version.py
+++ tests/test_version.py
@@ -5,28 +5,69 @@ import zstd
 
 from tests.base import BaseTestZSTD
 
-class TestVersions(BaseTestZSTD):
+MIN_LIBRARY_VERSION_NUMBER = 1*100*100 + 3*100 + 4
 
-    def test_module_version(self):
-        self.assertEqual(self.PKG_VERSION, zstd.version())
+class VersionNumbers(BaseTestZSTD):
+
+    def test_VERSION(self):
+        # zstd.VERSION should have four components,
+        # each of them a nonnegative integer
+        a, b, c, d = zstd.VERSION.split('.')
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        d = int(d)
+        self.assertTrue(a >= 0)
+        self.assertTrue(b >= 0)
+        self.assertTrue(c >= 0)
+        self.assertTrue(d >= 0)
+
+    def test_LIBRARY_VERSION(self):
+        # zstd.LIBRARY_VERSION should have three components,
+        # each of them a nonnegative integer
+        a, b, c = zstd.LIBRARY_VERSION.split('.')
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        self.assertTrue(a >= 0)
+        self.assertTrue(b >= 0)
+        self.assertTrue(c >= 0)
+
+    def test_LIBRARY_VERSION_NUMBER(self):
+        ver = zstd.LIBRARY_VERSION_NUMBER
+        self.assertTrue(ver >= MIN_LIBRARY_VERSION_NUMBER)
+
+        a, b, c = zstd.LIBRARY_VERSION.split('.')
+        a = int(a)
+        b = int(b)
+        c = int(c)
+        self.assertEqual(a*100*100 + b*100 + c, ver)
 
     def test_library_version(self):
-        if self.ZSTD_EXTERNAL:
-            self.skipTest("PyZstd was build with external version of "
-                          "ZSTD library (%s). It can be any version. Almost."
-                          % zstd.library_version())
+        a, b, c = zstd.LIBRARY_VERSION.split('.')
+        a = int(a)
+        b = int(b)
+        c = int(c)
 
-        self.assertEqual(self.VERSION, zstd.library_version())
+        x, y, z = zstd.library_version().split('.')
+        x = int(x)
+        y = int(y)
+        z = int(z)
+
+        self.assertTrue(x >= a)
+        self.assertTrue(x > a or y >= b)
+        self.assertTrue(x > a or y > a or z >= c)
 
     def test_library_version_number(self):
-        if self.ZSTD_EXTERNAL:
-            # Python 2.6 unittest missing assertLessEqual
-            self.assertFalse(self.VERSION_INT_MIN >
-                             zstd.library_version_number(),
-                             msg="PyZstd %s require external library "
-                             "version >= 1.0.0!" % zstd.version())
-        else:
-            self.assertEqual(self.VERSION_INT, zstd.library_version_number())
+        ver = zstd.library_version_number()
+        self.assertTrue(ver >= zstd.LIBRARY_VERSION_NUMBER)
+        self.assertTrue(ver >= MIN_LIBRARY_VERSION_NUMBER)
+
+        x, y, z = zstd.library_version().split('.')
+        x = int(x)
+        y = int(y)
+        z = int(z)
+        self.assertEqual(x*100*100 + y*100 + z, ver)
 
 if __name__ == "__main__":
     unittest.main()
