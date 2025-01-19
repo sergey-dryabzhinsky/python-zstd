532ba4e2a56e4bb863e707d84b8758b6ac6b269e
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2025-01-19 13:21:12 +0300

    Update test_speed.py
    Add new information functions

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -6,8 +6,8 @@ from time import time
 class TestZstdSpeed(BaseTestZSTD):
 
     def test_system_info(self):
-        log.info("Bundled libzstd uses assembler? :")
-        log.info("Bundled libzstd uses threads? :")
+        log.info("Bundled libzstd uses assembler? : %r" zstd.ZSTD_with_asm())
+        log.info("Bundled libzstd uses threads? :%r" %zstd.ZSTD_with_threads())
 
     def test_compressio_speed(self):
         log.info("Compression speed average. Wait 60 seconds...")
@@ -19,7 +19,7 @@ class TestZstdSpeed(BaseTestZSTD):
             cdata = zstd.compress(tDATA)
             sum+=l
 
-        log.info("Compression speed average =%.%6f b/sec" % (1.0*sum/sec,))
+        log.info("Compression speed average =%r b/sec" % (1.0*sum/sec,))
 
 if __name__ == '__main__':
     unittest.main()
