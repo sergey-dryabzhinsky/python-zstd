d7d82d46b389e6af11a47d18e0d9a53af80f0f43
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2025-01-19 13:53:31 +0300

    Update setup.py
    No threads by default

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ DISABLE_ASM=1
 if SUP_ASM:
      DISABLE_ASM=0
 
-SUP_THREADS="ZSTD_THREADS" in os.environ or True 
+SUP_THREADS="ZSTD_THREADS" in os.environ #or True 
 if "--libzstd-no-threads" in sys.argv:
     # Disable support multithreading in lizstd
     SUP_THREADS=False
