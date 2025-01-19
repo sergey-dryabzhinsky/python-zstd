f90fdc476d9cd3be2253a0fbc19ddaccf2640d6c
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2025-01-19 21:28:00 +0300

    Update test_decompress.py
    Update test

--- tests/test_decompress.py
+++ tests/test_decompress.py
@@ -12,8 +12,13 @@ class TestZstdDecompress(BaseTestZSTD):
             DATA =b''
         self.assertRaises(zstd.Error, zstd.uncompress, zstd.compress(DATA)+b' ')
 
-    def test_check(self):
+    def test_check_compressed(self):
         cdata = zstd.compress(tDATA)
         log.info("zstd compressed data check:%r" % zstd.check(cdata))
-        self.assertEqual(1, zstd.check, cdata)
+        self.assertEqual(1, zstd.check(cdata))
 
+    def test_check_uncompressed(self):
+        cdata = b'\0'
+        log.info("zstd uncompressed data check:%r" % zstd.check(cdata))
+        self.assertEqual(0, zstd.check(cdata))
+        
