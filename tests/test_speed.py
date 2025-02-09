9548301c2da93a0c89a01b02c5d7ed1c53e8b7c7
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2025-02-09 10:57:13 +0300

    Update test_speed.py

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -10,29 +10,29 @@ class TestZstdSpeed(BaseTestZSTD):
         log.info("Bundled libzstd uses threads? :%r" % zstd.ZSTD_with_threads())
 
     def test_compression_speed(self):
-        log.info("\nWait 60 seconds...")
-        sec = 60
+        wait = 30
+        log.info("\nWait %d seconds..." % wait)
         sum = 0
         l=len(tDATA)
         tbegin = time()
-        while time()-tbegin<sec:
+        while time()-tbegin<wait:
             cdata = zstd.compress(tDATA)
             sum+=l
 
-        log.info("Compression speed average =%6f Mb/sec" % (1.0*sum/1024/1024/sec,))
+        log.info("Compression speed average =%6f Mb/sec" % (1.0*sum/1024/1024/wait,))
 
     def test_decompression_speed(self):
-        log.info("\nWait 60 seconds...")
-        sec = 60
+        wait = 30
+        log.info("\nWait %d seconds..." % wait)
         sum = 0
         cdata = zstd.compress(tDATA)
         l=len(cdata)
         tbegin = time()
-        while time()-tbegin<sec:
+        while time()-tbegin<wait:
             data = zstd.decompress(cdata)
             sum+=l
 
-        log.info("Decompression speed average =%6f Mb/sec" % (1.0*sum/1024/1024/sec,))
+        log.info("Decompression speed average =%6f Mb/sec" % (1.0*sum/1024/1024/wait,))
 
     def test_check_speed(self):
         log.info("\nWait 60 seconds...")
