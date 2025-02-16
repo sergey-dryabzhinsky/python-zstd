ab8bba1a114d420e1c754b43a56bcd740364da2c
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2025-02-16 13:58:08 +0300

    Update python-zstd.c

--- src/python-zstd.c
+++ src/python-zstd.c
@@ -248,7 +248,7 @@ static PyObject *py_zstd_check(PyObject* self, PyObject *args)
     dest_size = (uint64_t) ZSTD_getFrameContentSize(source, source_size);
     if (dest_size == ZSTD_CONTENTSIZE_UNKNOWN || dest_size == ZSTD_CONTENTSIZE_ERROR) {
         //PyErr_Format(ZstdError, "Input data invalid or missing content size in frame header.");
-        return Py_BuildValue("i", 0);
+        return Py_BuildValue("i", -1);
     }
 
 	// Find real dest_size across multiple frames
