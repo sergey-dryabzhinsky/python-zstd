8b71dc1cb83b91d2460260965b23b90d24972f75
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2025-03-02 14:43:28 +0300

    Update python-zstd.c

--- src/python-zstd.c
+++ src/python-zstd.c
@@ -209,7 +209,7 @@ static PyObject *py_zstd_uncompress(PyObject* self, PyObject *args)
 		/*if (sizeof(uint64_t)==sizeof(unsigned long)) {
 			PyErr_Format(ZstdError, "Decompression error: length mismatch -> decomp %lu != %lu [header]", (uint64_t)cSize,  (uint64_t)dest_size);
 		} else { */
-                        PyErr_Format(ZstdError, "Decompression error: length mismatch -> decomp %llu != %llu [header]", (uint64_t)cSize,  (uint64_t)dest_size);
+                        PyErr_Format(ZstdError, "Decompression error: length mismatch -> decomp %lu != %lu [header]", (uint64_t)cSize,  (uint64_t)dest_size);
 		//}
             error = 1;
         }
