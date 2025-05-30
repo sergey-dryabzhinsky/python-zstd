1b190c4e5548df223a7226fe2b791c0192afc107
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2025-05-30 12:12:14 +0300

    Skip test on windows

--- tests/test_decompress.py
+++ tests/test_decompress.py
@@ -1,7 +1,7 @@
 # Tests
 
 import sys, os
-from tests.base import BaseTestZSTD, zstd, tDATA, log, raise_skip
+from tests.base import BaseTestZSTD, zstd, tDATA, log, raise_skip, plllatform
 
 class TestZstdDecompress(BaseTestZSTD):
 
@@ -13,7 +13,9 @@ class TestZstdDecompress(BaseTestZSTD):
         self.assertRaises(zstd.Error, zstd.uncompress, zstd.compress(DATA)+b' ')
 
     def test_decompression_streamed(self):
-        #log.info('cwd: %s' % os.getcwd())
+        log.info('debug windows cwd: %s' % os.getcwd())
+        if platform.system()=='Windows':
+            raise_skip("Windiws can't find tests data")
         f = open("tests/test_data/facebook.ico.zst","rb")
         DATA = f.read()
         f.close()
